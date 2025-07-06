343768f380918b9f182b2c70394da2213cad5dc5
Minwater <minsoo6505@naver.com>
2025-07-06 15:26:11 +0900

    docs: Todo 기능 Swagger 문서화 추가2

--- src/block/dtos/create-block.dto.ts
+++ src/block/dtos/create-block.dto.ts
@@ -1,15 +1,18 @@
-import { IsString, IsInt, IsDateString } from 'class-validator';
+import { IsString, IsInt } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateBlockDto {
-    @IsString()
-    url: string;
+  @ApiProperty({
+    example: 'https://www.youtube.com',
+    description: '차단할 웹사이트의 URL',
+  })
+  @IsString()
+  url: string;
 
-    @IsInt()
-    userId: number;
-
-    @IsDateString()
-    startTime: string;
-
-    @IsDateString()
-    endTime: string;
+  @ApiProperty({
+    example: 1,
+    description: '차단을 등록한 사용자 ID',
+  })
+  @IsInt()
+  userId: number;
 }
